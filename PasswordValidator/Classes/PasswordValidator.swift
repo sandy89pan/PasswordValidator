284f88c4a5016f89d2d12a60995d36280b769264
sandy.pan <sandy.pan@ringcentral.com>
2025-04-25 13:37:27 +0800

    misc: password validator

--- PasswordValidator/Classes/PasswordValidator.swift
+++ PasswordValidator/Classes/PasswordValidator.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class PasswordValidator {
     public static func isValid(password: String) -> Bool {
-        let pattern = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$"
+        let regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,12}$"
         let regex = try? NSRegularExpression(pattern: pattern)
         let range = NSRange(location: 0, length: password.utf16.count)
         return regex?.firstMatch(in: password, options: [], range: range) != nil
